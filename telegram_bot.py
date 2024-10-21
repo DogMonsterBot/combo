4d4c8bfd1b8f9bacaebf4c94d1a62e0436235ed2
Dog Monster <dogmonsterbot@gmail.com>
2024-10-21 14:52:40 +0330

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -11,18 +11,16 @@ logger = logging.getLogger(__name__)
 
 # آیدی کانال مقصد (کانالی که ربات در آن ادمین است)
 TARGET_CHANNEL_ID = '@IntroductionofAirdrop'  # کانال مقصد
-SOURCE_CHANNEL_USERNAME = '@gemz_combo_daily'  # کانال مبدا
+SOURCE_CHANNEL_USERNAME = '@MemefiCode'  # کانال مبدا
 
 # تابعی برای پردازش پست‌ها
-async def forward_combo_posts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def forward_posts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     message: Message = update.message
 
-    # بررسی وجود هشتگ #combo در متن پست
-    if '#combo' in message.text:
-        # حذف لینک‌ها از متن
-        modified_text = ' '.join(word for word in message.text.split() if not word.startswith('http'))
-
-        # ارسال پست به کانال هدف
+    # ارسال پست به کانال هدف بدون نمایش نام کانال
+    if message.text:  # اطمینان از وجود متن در پیام
+        # حذف نام کانال (در صورت وجود) و ارسال متن
+        modified_text = message.text.replace(SOURCE_CHANNEL_USERNAME, '').strip()
         await context.bot.send_message(chat_id=TARGET_CHANNEL_ID, text=modified_text)
 
 # راه‌اندازی ربات
@@ -30,11 +28,14 @@ async def main() -> None:
     application = ApplicationBuilder().token("8026191420:AAGkIwuskDtU_opshjhY5DRMRP72Gzg5ojU").build()
 
     # اضافه کردن هندلر برای پیام‌ها
-    application.add_handler(MessageHandler(filters.TEXT & filters.Chat(username=SOURCE_CHANNEL_USERNAME), forward_combo_posts))
+    application.add_handler(MessageHandler(filters.TEXT & filters.Chat(username=SOURCE_CHANNEL_USERNAME), forward_posts))
 
     # شروع ربات
     await application.run_polling()
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(main())
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(main())
+    loop.run_forever()
