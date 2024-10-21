6d47d4815bf7a766bcb42c533c6c29152894a043
Dog Monster <dogmonsterbot@gmail.com>
2024-10-21 22:42:54 +0330

    Add files via upload

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,6 +1,10 @@
 import logging
+import nest_asyncio
 from telegram import Update, Message
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+# فعال کردن nest_asyncio
+nest_asyncio.apply()
 
 # فعال کردن لاگینگ
 logging.basicConfig(
@@ -11,7 +15,9 @@ logger = logging.getLogger(__name__)
 
 # آیدی کانال مقصد (کانالی که ربات در آن ادمین است)
 TARGET_CHANNEL_ID = '@IntroductionofAirdrop'  # کانال مقصد
-SOURCE_CHANNEL_USERNAME = '@MemefiCode'  # کانال مبدا
+
+# لیستی از کانال‌های مبدا
+SOURCE_CHANNEL_USERNAMES = ['@gemz_combo_daily', '@MemefiCode', '@DogMonster']  # کانال‌های مبدا
 
 # تابعی برای پردازش پست‌ها
 async def forward_posts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -19,23 +25,25 @@ async def forward_posts(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
 
     # ارسال پست به کانال هدف بدون نمایش نام کانال
     if message.text:  # اطمینان از وجود متن در پیام
-        # حذف نام کانال (در صورت وجود) و ارسال متن
-        modified_text = message.text.replace(SOURCE_CHANNEL_USERNAME, '').strip()
+        # حذف نام کانال‌های مبدا از متن
+        modified_text = message.text
+        for channel_username in SOURCE_CHANNEL_USERNAMES:
+            modified_text = modified_text.replace(channel_username, '').strip()
+
+        # ارسال متن اصلاح شده به کانال مقصد
         await context.bot.send_message(chat_id=TARGET_CHANNEL_ID, text=modified_text)
 
 # راه‌اندازی ربات
 async def main() -> None:
     application = ApplicationBuilder().token("8026191420:AAGkIwuskDtU_opshjhY5DRMRP72Gzg5ojU").build()
 
-    # اضافه کردن هندلر برای پیام‌ها
-    application.add_handler(MessageHandler(filters.TEXT & filters.Chat(username=SOURCE_CHANNEL_USERNAME), forward_posts))
+    # اضافه کردن هندلر برای پیام‌ها از همه کانال‌های مبدا
+    for channel_username in SOURCE_CHANNEL_USERNAMES:
+        application.add_handler(MessageHandler(filters.TEXT & filters.Chat(username=channel_username), forward_posts))
 
     # شروع ربات
     await application.run_polling()
 
 if __name__ == '__main__':
     import asyncio
-
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
-    loop.run_forever()
+    asyncio.run(main())
