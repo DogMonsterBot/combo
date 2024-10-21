b58cf7ae62b86da158bfb71e4853124d14200160
Dog Monster <dogmonsterbot@gmail.com>
2024-10-22 01:55:39 +0330

    Add files via upload

--- pro bot.py
+++ pro bot.py
@@ -1,34 +1,35 @@
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
-import nest_asyncio  # اضافه کنید
 
 # توکن ربات
 BOT_TOKEN = '8026191420:AAGkIwuskDtU_opshjhY5DRMRP72Gzg5ojU'
 
-# شناسه کانال مبدا و مقصد
-SOURCE_CHANNEL_IDS = [
-    '7320989848',
-    '-1002215159433',
-    '-1002490406722'
-]
-TARGET_CHANNEL_ID = '-1002248091563'
+# شناسه گروه مقصد
+TARGET_GROUP_ID = '@IntroductionofAirdrop'
+
+# هشتگ‌های مورد نظر
+TAGS = ['#tag1', '#tag2', '#tag3']  # اینجا می‌توانید هشتگ‌های دلخواه خود را اضافه کنید
 
 # راه‌اندازی logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 # تابع برای پردازش پیام‌ها
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # چک کردن اینکه آیا پیام از کانال مبدا آمده است
-    if str(update.message.chat.id) in SOURCE_CHANNEL_IDS:
+    # چک کردن اینکه آیا پیام شامل هشتگ‌های مشخص شده است
+    if any(tag in update.message.text for tag in TAGS):
         # حذف لینک‌ها از متن
         text = update.message.text
         text_without_links = ' '.join(word for word in text.split() if not word.startswith('http'))
 
         try:
-            # ارسال به کانال مقصد
-            await context.bot.send_message(chat_id=TARGET_CHANNEL_ID, text=text_without_links)
-            logging.info(f"Message sent to target channel: {text_without_links}")
+            # ارسال متن یا تصویر به گروه مقصد
+            if update.message.photo:  # چک کردن وجود تصویر
+                await context.bot.send_photo(chat_id=TARGET_GROUP_ID, photo=update.message.photo[-1].file_id, caption=text_without_links)
+            else:
+                await context.bot.send_message(chat_id=TARGET_GROUP_ID, text=text_without_links)
+
+            logging.info(f"Message sent to target group: {text_without_links}")
         except Exception as e:
             logging.error(f"Failed to send message: {e}")
 
@@ -38,6 +39,7 @@ async def main():
 
     # افزودن هندلر برای پیام‌ها
     application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
+    application.add_handler(MessageHandler(filters.PHOTO, handle_message))  # اضافه کردن هندلر برای تصاویر
 
     # شروع polling
     await application.run_polling()
@@ -45,11 +47,8 @@ async def main():
 if __name__ == '__main__':
     import asyncio  # فقط در اینجا وارد کنید
 
-    # وارد کردن nest_asyncio
-    nest_asyncio.apply()  # این خط را اضافه کنید
-
     # ثبت شروع ربات
     logging.info("Starting the bot...")
-
+    
     # استفاده از حلقه‌ی رویداد در حال اجرا
     asyncio.run(main())
